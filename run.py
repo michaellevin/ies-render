e75d5e61367e08824bd56ca4c2b5c18e9b3491d9
Michael Levin <michael.levi.fx@gmail.com>
2023-09-28 02:01:27 +0300

    launch fix

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from module import IES_Thumbnail_Generator
 
-ies_paths = [
-    "examples/vertical_angles.ies",
-    "examles/horiz_angles.ies",
-    "examples/ies-lights-pack/area-light.ies",
-]
-tb = IES_Thumbnail_Generator(ies_paths[2])
-# tb.generate(size=1024, horizontal_angle=0, distance=0.3, blur_radius=0.5, save=True)
-tb.render(size=1024, horizontal_angle=0, distance=0.0, blur_radius=0, save=True)
+if __name__ == "__main__":
+    ies_paths = [
+        "examples/vertical_angles.ies",
+        "examles/horiz_angles.ies",
+        "examples/ies-lights-pack/area-light.ies",
+    ]
+    tb = IES_Thumbnail_Generator(ies_paths[2])
+    # tb.generate(size=1024, horizontal_angle=0, distance=0.3, blur_radius=0.5, save=True)
+    tb.render(size=1024, horizontal_angle=0, distance=0.0, blur_radius=0, save=True)
